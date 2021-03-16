8ef6c7a61a3bc05ea37785a0e2b6f1e79ea61ac1
kmakoto0212 <70053303+kmakoto0212@users.noreply.github.com>
2021-03-16 11:17:02 +0900

    Revert "fix:  Supports array of array type to Cell.value"
    This reverts commit a73707f923a7c14ed6e86069edd185e60b25007f.

--- index.d.ts
+++ index.d.ts
@@ -138,7 +138,7 @@ declare namespace XlsxPopulate {
     style(name: any[][]): Range
     style(styles: {[key: string]: any}): Cell
     style(style: Style): Cell
-    value(): string | boolean | number | Date | undefined | (string | boolean | number | Date | undefined)[][]
+    value(): string | boolean | number | Date | undefined
     value(value: string | boolean | number | null | undefined): Cell
     value(): Range
     workbook(): Workbook
